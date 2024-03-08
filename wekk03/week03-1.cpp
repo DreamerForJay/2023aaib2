0a7dd5c0e7caa1ec8e3898c60206ad418ee8e464
DreamerForJay <cl.yang04@gmail.com>
2024-03-08 10:06:44 +0800

    week03

---
+++ wekk03/week03-1.cpp
@@ -0,0 +1,9 @@
+///week03-1.cpp
+#include<stdio.h>
+int main(){
+    int a = 5;
+    ///if(a%2==1) printf("odd\n");///do 1
+    while(a%2==1) printf("odd\n");
+    while(a>0) printf("a is > 0\n");
+
+}
