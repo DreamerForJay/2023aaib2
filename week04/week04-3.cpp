e5a99774d7a632ece0e1501be906c5a2cb5226a8
DreamerForJay <cl.yang04@gmail.com>
2024-03-15 10:39:34 +0800

    week04

---
+++ week04/week04-3.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+  for(int i=1; i<=9;i++){ ///進階型,從1開始(人類理解)
+    printf("i是%d ", i);
+  }
+  printf("\n");
+  for(int i=9;i>=1;i--){
+      printf("i是%d ", i);
+  }
+}
