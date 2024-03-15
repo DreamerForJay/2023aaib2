d18ccfbf2e47dc2d5ecf466bf3fbb7c66388c870
DreamerForJay <cl.yang04@gmail.com>
2024-03-15 15:34:11 +0800

    week04

---
+++ week04/week04-1.py
@@ -0,0 +1,3 @@
+# week04-1 基礎型 從1開始 電腦理解
+for i in range(0,3):
+  print('i是',i)
---
+++ week04/week04-5Java.cpp
@@ -0,0 +1,16 @@
+///week04-5.cpp CPE-0102A,看不同的排版法
+///Allman(ANST)、Java、K&C(C發明者)、Stoustrup(C++排版法)
+#include<stdio.h>
+int main() {
+    int n,a,b;
+    scanf("%d",&n);
+    for(int i=0; i<n; i++) {
+        scanf("%d",&a);
+        int ans=0;
+        for(int j=0; j<a; j++) {
+            scanf("%d",&b);
+            ans+=b;
+        }
+        printf("%d\n",ans);
+    }
+}
